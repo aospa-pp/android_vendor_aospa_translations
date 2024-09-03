1bda0b39c1eccedf901d51f2f1ffae31c83377ed
songlongdaiduong <song.long.dai.duong@gmail.com>
2024-09-03 14:14:54 +0800

    Remove translation for ParanoidSense
    Included in ParanoidSense now
    
    Change-Id: I924e3280e031a7ce25b553460279e8b66e9fb616

--- translations.mk
+++ translations.mk
@@ -22,6 +22,5 @@ PRODUCT_PACKAGES += \
     ParanoidHubTranslation \
     ParanoidLauncherTranslation \
     ParanoidPapersTranslation \
-    ParanoidSenseTranslation \
     ParanoidSystemUITranslation \
     ParanoidThemePickerTranslation
